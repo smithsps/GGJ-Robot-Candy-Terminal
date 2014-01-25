d49c29348a849180615b6ea71692d2637765335e
Simon Smith <smithsps@gmail.com>
2014-01-24 21:20:47 -0500

    Simple Layout
    with States!

---
+++ Assets/UI/UI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+
+	public Texture OverlordLayoutTexture;
+	public Texture PeterLayoutTexture;
+
+	public bool state = true;
+
+	// Use this for initialization
+	void Start () {
+		//Get Player state
+		//state = true;
+	}
+	
+	// Update is called once per frame
+	void OnGUI() {
+		if(state) {
+			OverlordLayout();
+		} else {
+			PeterLayout();
+		}
+	}
+
+	void PeterLayout() {
+		GUI.DrawTexture(new Rect(0,0,1280,720), PeterLayoutTexture);
+	}
+
+	void OverlordLayout(){
+		GUI.DrawTexture(new Rect(0,0,1280,720), OverlordLayoutTexture);
+	}
+}
