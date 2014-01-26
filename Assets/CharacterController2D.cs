6463c0ebf0f750fb76791a88eb818f528014f469
BobLoblaw4444 <twolvert@purdue.edu>
2014-01-26 01:08:50 -0500

    Animations!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    Added em

--- Assets/CharacterController2D.cs
+++ Assets/CharacterController2D.cs
@@ -17,60 +17,71 @@ public class CharacterController2D : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if(Network.isClient)
+		if(true)//Network.isClient)
 		{
 			float xDir = Input.GetAxis("Horizontal");
 			float yDir = Input.GetAxis("Vertical");
+
+			//if(xDir > 0)
+			//	GetComponent<Animator>().SetBool("MoveRight", true);
 
 			RotateSprite(xDir, yDir);
 
 			Vector3 moveDir = new Vector3(xDir, yDir, 0);
 
-			transform.Translate(speed * moveDir * Time.deltaTime);
+			transform.Translate(speed * moveDir * Time.deltaTime, Space.World);
 		}
 	}
 
 	void RotateSprite(float xDir, float yDir)
 	{
-		if(xDir > 0)
+		if(xDir < 0)
 		{
 			if(yDir > 0)
 			{
-				transform.Rotate(new Vector3(0, 0, 45));
+				GetComponent<Animator>().SetInteger("RotationDirection", 1);
+				//transform.Rotate(new Vector3(0, 0, 45));
 			}
 			else if(yDir < 0)
 			{
-				transform.Rotate(new Vector3(0, 0, 135));
+				GetComponent<Animator>().SetInteger("RotationDirection", 3);
+				//transform.Rotate(new Vector3(0, 0, 135));
 			}
 			else
 			{
-				transform.Rotate(new Vector3(0, 0, 90));
+				GetComponent<Animator>().SetInteger("RotationDirection", 2);
+				//transform.Rotate(new Vector3(0, 0, 90));
 			}
 		}
-		else if(xDir < 0)
+		else if(xDir > 0)
 		{
 			if(yDir > 0)
 			{
-				transform.Rotate(new Vector3(0, 0, 315));
+				GetComponent<Animator>().SetInteger("RotationDirection", 7);
+				//transform.Rotate(new Vector3(0, 0, 315));
 			}
 			else if(yDir < 0)
 			{
-				transform.Rotate(new Vector3(0, 0, 225));
+				GetComponent<Animator>().SetInteger("RotationDirection", 5);
+				//transform.Rotate(new Vector3(0, 0, 225));
 			}
 			else
 			{
-				transform.Rotate(new Vector3(0, 0, 270));
+				GetComponent<Animator>().SetInteger("RotationDirection", 6);
+				//transform.Rotate(new Vector3(0, 0, 270));
 			}
 		}
 		else
 		{
 			if(yDir > 0)
 			{
-				transform.Rotate(new Vector3(0, 0, 0));
+				GetComponent<Animator>().SetInteger("RotationDirection", 0);
+				//transform.Rotate(new Vector3(0, 0, 0));
 			}
 			else if(yDir < 0)
 			{
-				transform.Rotate(new Vector3(0, 0, 180));
+				GetComponent<Animator>().SetInteger("RotationDirection", 4);
+				//transform.Rotate(new Vector3(0, 0, 180));
 			}
 		}
 	}
